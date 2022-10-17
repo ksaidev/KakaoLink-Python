ada4fba3e8a0ce89f927dad4c58108a7a6f088f0
정재엽 <jjy37777@naver.com>
2022-10-17 22:49:07 +0900

    Fix : 2022년 10월 기준

--- KakaoModule.py
+++ KakaoModule.py
@@ -1,204 +1,215 @@
 import requests, json, urllib, base64
 from bs4 import BeautifulSoup
+
 BS = BeautifulSoup
 from Crypto import Random
 from Crypto.Cipher import AES
 from hashlib import md5
+from urllib import parse
+import base64
+
 
 class Kakao:
-    def __init__ (self, apiKey, your_site_address):
-        if not isinstance (apiKey, str) or len (apiKey) != 32:
+    def __init__(self, apiKey, your_site_address):
+        if not isinstance(apiKey, str) or len(apiKey) != 32:
             raise TypeError("API 키 ( " + str(apiKey) + " ) 가 올바르지 않습니다.")
         self.apiKey = apiKey
+        self.shortKey = None
         self.cookies = {}
         self.loginReferer = None
         self.cryptoKey = None
         self.parsedTemplate = None
-        self.csrf = None
+        self.checksum = None
+        self.rooms = None
         self.static = {
             'ua': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
             'ct': 'application/x-www-form-urlencoded',
-            'ka': 'sdk/1.36.6 os/javascript lang/en-US device/Win32 origin/'+urllib.parse.quote_plus(your_site_address)
+            'ka': 'sdk/1.36.6 os/javascript lang/en-US device/Win32 origin/' + urllib.parse.quote_plus(
+                your_site_address)
         }
 
     def AES_encrypt(self, message, passphrase):
+
+        BLOCK_SIZE = 16
+
+        def pad(data):
+            length = BLOCK_SIZE - (len(data) % BLOCK_SIZE)
+            return data + (chr(length) * length).encode()
+
+        def bytes_to_key(data, salt, output=48):
+            assert len(salt) == 8, len(salt)
+            data += salt
+            key = md5(data).digest()
+            final_key = key
+            while len(final_key) < output:
+                key = md5(key + data).digest()
+                final_key += key
+            return final_key[:output]
 
         message = bytes(message, 'utf-8')
         passphrase = bytes(passphrase, 'utf-8')
 
         salt = Random.new().read(8)
-        
-        passphrase += salt
-        key = md5(passphrase).digest()
-        final_key = key
-        while len(final_key) < 48:
-                key = md5(key + passphrase).digest()
-                final_key += key
-        key_iv = final_key[:48]
-
+        key_iv = bytes_to_key(passphrase, salt, 32 + 16)
         key = key_iv[:32]
         iv = key_iv[32:]
         aes = AES.new(key, AES.MODE_CBC, iv)
 
-        #pading
-        length = 16 - (len(message) % 16)
-        padded_message = message + (chr(length)*length).encode()
-
-        return base64.b64encode(b"Salted__" + salt + aes.encrypt(padded_message)).decode("utf-8") 
-
-    def loginManager(self):
-        connect = requests.post(
-            'https://sharer.kakao.com/talk/friends/picker/link',
-            headers = {'User-Agent' : self.static['ua']},
-            data = {
-                'app_key' : self.apiKey,
-                'validation_action' : 'default',
-                'validation_params' : '{}',
-                'ka' : self.static['ka'],
-                'lcba' : ''
-            }
-        )
-
-        status = connect.status_code
-        if status == 401:
-            raise ValueError('API 키가 유효하지 않습니다.')
-        if status != 200:
-            raise Exception('예상치 못한 오류로 로그인에 실패했습니다.')
-
-        self.cookies['_kadu'] = connect.cookies.get('_kadu')
-        self.cookies['_kadub'] = connect.cookies.get('_kadub')
-        self.cookies['_maldive_oauth_webapp_session'] = connect.cookies.get('_maldive_oauth_webapp_session')
-        self.loginReferer = connect.url
-        ctx = BS(connect.content, 'html.parser')
-        self.cryptoKey = ctx.find('input', {'name' : 'p'}).get('value')
-
-    def tiara(self):
-        tiara = requests.post(
-            'https://track.tiara.kakao.com/queen/footsteps'
-        ).cookies.get('TIARA')
-        self.cookies['TIARA'] = tiara
-
-    def authenticate(self,ID, PW):
-        connect = requests.post(
-            'https://accounts.kakao.com/weblogin/authenticate.json',
-            headers = {
-                'User-Agent' : self.static['ua'],
-                'Referer' : self.loginReferer
-            },
-            cookies = self.cookies,
-            data = {
-                'os' : 'web',
-                'webview_v' : '2',
-                'email' : self.AES_encrypt(ID, self.cryptoKey),
-                'password' : self.AES_encrypt(PW, self.cryptoKey),
-                'continue': self.loginReferer.split('continue=')[1],
-                'third': 'false',
-                'k': 'true',
-            }
-        )
-        result = json.loads(connect.text)
-        if result['status'] == -450:
-            raise ValueError("아이디나 비밀번호가 잘못 되었습니다.")
-        elif result['status'] != 0:
-            print(result)
-            raise Exception(str(result['status']) + ' > 예상치 못한 오류로 로그인에 실패했습니다.')
-
-        self.cookies['_kawlt'] = connect.cookies.get('_kawlt')
-        self.cookies['_kawltea'] = connect.cookies.get('_kawltea')
-        self.cookies['_karmt'] = connect.cookies.get('_karmt')
-        self.cookies['_karmtea'] = connect.cookies.get('_karmtea')
+        return base64.b64encode(
+            b"Salted__" + salt + aes.encrypt(pad(message))).decode("utf-8")
 
     def login(self, ID, PW):
-        self.loginManager()
-        self.tiara()
-        self.authenticate(ID, PW)
+        if not isinstance(ID, str):
+            raise TypeError("아이디의 타입 " + type(ID) + " 가 올바르지 않습니다.")
+        elif not isinstance(PW, str):
+            raise TypeError("비밀번호의 타입 " + type(PW) + "가 올바르지 않습니다.")
 
-    def proceed(self, data, sendtype):
-        connect = requests.post(
-            'https://sharer.kakao.com/talk/friends/picker/link',
-            headers = {
-                'User-Agent' : self.static['ua'],
-                'Referer' : self.loginReferer
-            },
-            cookies = {
-                'TIARA' : self.cookies['TIARA'],
-                '_kawlt' : self.cookies['_kawlt'],
-                '_kawltea' : self.cookies['_kawltea'],
-                '_karmt' : self.cookies['_karmt'],
-                '_karmtea' : self.cookies['_karmtea']
-            },
-            data = {
-                'app_key' : self.apiKey,
-                'validation_action' : sendtype,
-                'validation_params' : json.dumps(data),
-                'ka' : self.static['ka'],
-                'lcba' : ''
-            }
-        )
+        def loginManager():
+            connect = requests.post(
+                'https://sharer.kakao.com/talk/friends/picker/link',
+                headers={'User-Agent': self.static['ua']},
+                data={
+                    'app_key': self.apiKey,
+                    'validation_action': 'default',
+                    'validation_params': '{}',
+                    'ka': self.static['ka'],
+                    'lcba': ''
+                }
+            )
 
-        if connect.status_code == 400:
-            raise TypeError('템플릿의 변수가 올바르지 않습니다.')
-        self.cookies['KSHARER'] = connect.cookies.get('KSHARER')
-        self.cookies['using'] = 'true'
-        
-        ctx = BS(connect.content, 'html.parser')
+            status = connect.status_code
+            if status == 401:
+                raise ValueError('API 키가 유효하지 않습니다.')
+            if status != 200:
+                raise Exception('예상치 못한 오류로 로그인에 실패했습니다.')
 
-        self.parsedTemplate = json.loads(ctx.find('input', {'id' : 'validatedTalkLink'}).get('value'))
-        self.csrf = ctx.find_all('div')[-1].get('ng-init').split('\'')[1]
+            self.cookies['_kadu'] = connect.cookies.get('_kadu')
+            self.cookies['_kadub'] = connect.cookies.get('_kadub')
+            self.cookies[
+                '_maldive_oauth_webapp_session_key'] = connect.cookies.get(
+                '_maldive_oauth_webapp_session_key')
+            self.loginReferer = connect.url
+            ctx = BS(connect.content, 'html.parser')
+            self.cryptoKey = ctx.find('input', {'name': 'p'}).get('value')
 
-    def getRooms(self):
-        connect = requests.get(
-            'https://sharer.kakao.com/api/talk/chats',
-            headers = {
-                'User-Agent' : self.static['ua'],
-                'Referer' : 'https://sharer.kakao.com/talk/friends/picker/link',
-                'Csrf-Token' : self.csrf,
-                'App-Key' : self.apiKey
-            },
-            cookies = self.cookies
-        )
-        document = connect.text.replace(u'\u200b','')
-        self.rooms = json.loads(document)
+        def tiara():
+            tiara = requests.get(
+                'https://stat.tiara.kakao.com/track?d=%7B%22sdk%22%3A%7B%22type%22%3A%22WEB%22%2C%22version%22%3A%221.1.15%22%7D%7D'
+            ).cookies.get('TIARA')
+            self.cookies['TIARA'] = tiara
 
+        def authenticate():
+            connect = requests.post(
+                'https://accounts.kakao.com/weblogin/authenticate.json',
+                headers={
+                    'User-Agent': self.static['ua'],
+                    'Referer': self.loginReferer
+                },
+                cookies=self.cookies,
+                data={
+                    'os': 'web',
+                    'webview_v': '2',
+                    'email': self.AES_encrypt(ID, self.cryptoKey),
+                    'password': self.AES_encrypt(PW, self.cryptoKey),
+                    'continue': parse.unquote(
+                        self.loginReferer.split('continue=')[1]),
+                    'third': 'false',
+                    'k': 'true',
+                }
+            )
+            result = json.loads(connect.text)
+            if result['status'] == -450:
+                raise ValueError("아이디나 비밀번호가 잘못 되었습니다.")
+            elif result['status'] != 0:
+                raise Exception(
+                    str(result['status']) + ' > 예상치 못한 오류로 로그인에 실패했습니다.')
 
-    def sendTemplate(self, roomTitle):
-        id, securityKey = None, None
-        for room in self.rooms['chats']:
-            if(room['title'] == roomTitle):
-                id = room['id']
-                securityKey = self.rooms['securityKey']
-                break
-        if not id: raise ReferenceError('invalid room name ' + roomTitle)
-        connect = requests.post(
-            'https://sharer.kakao.com/api/talk/message/link',
-            headers = {
-                'User-Agent' : self.static['ua'],
-                'Referer' : 'https://sharer.kakao.com/talk/friends/picker/link',
-                'Csrf-Token' : self.csrf,
-                'App-Key' : self.apiKey,
-                'Content-Type' : 'application/json;charset=UTF-8'
-            },
-            cookies = {
-                'KSHARER': self.cookies['KSHARER'],
-                'TIARA': self.cookies['TIARA'],
-                'using': self.cookies['using'],
-                '_kadu': self.cookies['_kadu'],
-                '_kadub': self.cookies['_kadub'],
-                '_kawlt': self.cookies['_kawlt'],
-                '_kawltea': self.cookies['_kawltea'],
-                '_karmt': self.cookies['_karmt'],
-                '_karmtea': self.cookies['_karmtea']
-            },
-            data = json.dumps({
-                'receiverChatRoomMemberCount': [1],
-                'receiverIds': [id],
-                'receiverType': 'chat',
-                'securityKey': securityKey,
-                'validatedTalkLink': self.parsedTemplate
-            }).encode('utf-8')
-        )
+            self.cookies['_kawlt'] = connect.cookies.get('_kawlt')
+            self.cookies['_kawltea'] = connect.cookies.get('_kawltea')
+            self.cookies['_karmt'] = connect.cookies.get('_karmt')
+            self.cookies['_karmtea'] = connect.cookies.get('_karmtea')
+
+        loginManager()
+        tiara()
+        authenticate()
 
     def send(self, roomTitle, data, sendtype='default'):
-        self.proceed(data, sendtype)
-        self.getRooms()
-        self.sendTemplate(roomTitle)
+        def proceed():
+
+            connect = requests.post(
+                'https://sharer.kakao.com/picker/link',
+                headers={
+                    'User-Agent': self.static['ua'],
+                    'Referer': self.loginReferer
+                },
+                cookies={
+                    'TIARA': self.cookies['TIARA'],
+                    '_kawlt': self.cookies['_kawlt'],
+                    '_kawltea': self.cookies['_kawltea'],
+                    '_karmt': self.cookies['_karmt'],
+                    '_karmtea': self.cookies['_karmtea']
+                },
+                data={
+                    'app_key': self.apiKey,
+                    'validation_action': sendtype,
+                    'validation_params': json.dumps(data),
+                    'ka': self.static['ka'],
+                    'lcba': ''
+                }
+            )
+
+            if connect.status_code == 400:
+                raise TypeError('템플릿의 변수가 올바르지 않습니다.')
+
+            encodedData = \
+                connect.content.decode("utf8").strip().split("serverData = \"")[1].split("\";")[0]
+            encodedData = f"{encodedData}{'=' * (len(encodedData) % 4)}"
+            decodedData = json.loads(
+                base64.b64decode(encodedData).decode("utf8"))
+
+            self.shortKey = decodedData["data"]["shortKey"]
+            self.csrfToken = decodedData["data"]["csrfToken"]
+            self.checksum = decodedData["data"]["checksum"]
+            self.rooms = decodedData["data"]["chats"]
+            self.cookies["_csrf"] = connect.cookies.get('_csrf')
+
+        proceed()
+
+        def sendTemplate():
+            receiver = None
+            for room in self.rooms:
+                if room['title'] == roomTitle:
+                    receiver = base64.b64encode(
+                        json.dumps(room, ensure_ascii=False).encode(
+                            "utf8")).decode("utf8")
+                    break
+            if not receiver: raise ReferenceError(
+                'invalid room name ' + roomTitle)
+
+            connect = requests.post(
+                'https://sharer.kakao.com/picker/send',
+                headers={
+                    'User-Agent': self.static['ua'],
+                    'Referer': 'https://sharer.kakao.com/talk/friends/picker/link',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                cookies={
+                    'TIARA': self.cookies['TIARA'],
+                    '_kadu': self.cookies['_kadu'],
+                    '_kadub': self.cookies['_kadub'],
+                    '_kawlt': self.cookies['_kawlt'],
+                    '_kawltea': self.cookies['_kawltea'],
+                    '_karmt': self.cookies['_karmt'],
+                    '_karmtea': self.cookies['_karmtea'],
+                    '_csrf': self.cookies['_csrf']
+                },
+                data={
+                    "app_key": self.apiKey,
+                    "short_key": self.shortKey,
+                    "_csrf": self.csrfToken,
+                    "checksum": self.checksum,
+                    "receiver": receiver
+                }
+            )
+
+        sendTemplate()
--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # KakaoLink-Python
 
-`pip install kaling`
-
-https://pypi.org/project/kaling/
-
 Send KakaoLink with pure Python
 
-Fork of https://github.com/pl-Steve28-lq/KakaoLink-For-Python with performance and safety improvements
-
-외부 사이트에서 encryption을 하면 제 비번으로 뭘하는지를 몰라서ㅜㅠ 보안과 속도를 위해 AES encryption을 외부 사이트가 아닌 내부에서 파이썬으로 하는 형식으로 고쳤습니다
-이제 거의 즉시 메시지가 전송됩니다!
-
-그리고 버튼에 걸수 있는 링크는 카카오데브에서 설정한 경로 내에 있어야 합니다
+2022년 10월 17일 기준 정상작동합니다!
 
 ### example
 
-    from kaling import Kakao
+    # from kaling import Kakao
+    from KakaoModule import Kakao
 
     KakaoLink = Kakao('javascript key','website written on you kaling')
     KakaoLink.login('login email', 'login password')
